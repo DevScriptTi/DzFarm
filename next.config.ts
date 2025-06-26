5eb06ab6d3526c67f11d87e3f1b9e536ac53e87a
Djaafri Fouzi <00.dev.00.dev.00@gmail.com>
2025-06-26 16:38:42 +0100

    final

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
+    // remotePatterns: [
+    //   {
+    //     protocol: 'http',
+    //     hostname: '127.0.0.1',
+    //     port: '8000',
+    //     pathname: '/storage/qrcodes/**',
+    //   },
+    //   // You can add more patterns if needed
+    // ],
+     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
+        protocol: 'https',
+        hostname: 'https://farm-back-end-main-lmyfff.laravel.cloud/',
+        // port: '8000',
         pathname: '/storage/qrcodes/**',
       },
       // You can add more patterns if needed
